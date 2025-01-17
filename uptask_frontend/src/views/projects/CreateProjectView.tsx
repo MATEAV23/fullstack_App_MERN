9359cc486519977c10fe2000a4f2f2a39c187540
MATEAV23 <mateogalvarezvic23@gmail.com>
2025-01-17 15:30:55 -0300

    se agrego notificacion de creacion de proyecto

--- uptask_frontend/src/views/projects/CreateProjectView.tsx
+++ uptask_frontend/src/views/projects/CreateProjectView.tsx
@@ -1,5 +1,6 @@
 import { Link, useNavigate } from "react-router-dom"
 import { useForm } from 'react-hook-form'
+import { toast } from 'react-toastify'
 import ProjectForm from "@/components/projects/ProjectForm"
 import { ProjectFormData } from "@/types/index"
 import { createProject } from "@/api/ProjectApi"
@@ -16,8 +17,9 @@ const CreateProjectView = () => {
 
     const { register, handleSubmit, formState: { errors } } = useForm({ defaultValues: initialValues })
 
-    const handleForm = async (data : ProjectFormData) => {
-        await createProject(data)
+    const handleForm = async (formData : ProjectFormData) => {
+        const data = await createProject(formData)
+        toast.success(data)
         navigate('/')
     }
 
