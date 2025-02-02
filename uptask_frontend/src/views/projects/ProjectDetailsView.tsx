02f5ab5c6e026cdbb18516595f770af5c366f7e2
MATEAV23 <mateogalvarezvic23@gmail.com>
2025-02-02 15:58:58 -0300

    manejando visualizacion de las tareas

--- uptask_frontend/src/views/projects/ProjectDetailsView.tsx
+++ uptask_frontend/src/views/projects/ProjectDetailsView.tsx
@@ -5,6 +5,7 @@ import { getProjectById } from '@/api/ProjectApi'
 import AddTaskModal from '@/components/tasks/AddTaskModal'
 import TaskList from '@/components/tasks/TaskList'
 import EditTaskData from '@/components/tasks/EditTaskData'
+import TaskModalDetails from '@/components/tasks/TaskModalDetails'
 
 const ProjectDetailsView = () => {
 
@@ -38,6 +39,7 @@ const ProjectDetailsView = () => {
             />
             <AddTaskModal/>
             <EditTaskData/>
+            <TaskModalDetails/>
         </>
     )
 }
