9a2e3c8606f89e95644822ef587ed930f77ca39a
MATEAV23 <mateogalvarezvic23@gmail.com>
2025-02-03 21:10:32 -0300

    Agregando select para cambio de estado

--- uptask_frontend/src/components/tasks/TaskList.tsx
+++ uptask_frontend/src/components/tasks/TaskList.tsx
@@ -1,5 +1,6 @@
 import type { Task } from "@/types/index"
 import TaskCard from "./TaskCard"
+import { statusTranslations } from "@/locales/es"
 
 type TaskListProps = {
     tasks: Task[]
@@ -18,13 +19,7 @@ const initialStatusGroups: GroupedTasks = {
 
 }
 
-const statusTranslations : {[key: string]: string} = {
-    pending: 'Pendiente',
-    onHold: 'En espera',
-    inProgress: 'En progreso',
-    underReview: 'En revisión',
-    completed: 'Completado'
-}
+
 
 const statusStyles : {[key: string]: string} = {
     pending: 'border-t-slate-500',
