05d22bcc8db4e921b8a8a9ebeacbea4a9e0a1073
MATEAV23 <mateogalvarezvic23@gmail.com>
2025-02-04 21:31:55 -0300

    creando schemma de usuario

---
+++ uptask_backend/src/models/User.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema, Document } from "mongoose"
+
+export interface IUser extends Document {
+    name: string
+    email: string
+    password: string
+    confirm: boolean
+}
+
+const userSchema : Schema = new Schema({
+    email : {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true
+    },
+    password : {
+        type: String,
+        required: true
+    },
+    name : {
+        type: String,
+        required: true
+    },
+    confirm : {
+        type: Boolean,
+        default: false
+    }
+}) 
+
+const User = mongoose.model<IUser>('User', userSchema)
+export default User
