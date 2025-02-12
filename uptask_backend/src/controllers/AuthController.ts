5d1e244f53f53b6add2231f5eeae876d9429c11a
MATEAV23 <mateogalvarezvic23@gmail.com>
2025-02-11 21:22:54 -0300

    creando chequeo de usuarios duplicados

--- uptask_backend/src/controllers/AuthController.ts
+++ uptask_backend/src/controllers/AuthController.ts
@@ -6,9 +6,16 @@ export class AuthController {
     
     static createAccount = async (req: Request, res: Response) => {
         try {
-            const {password} = req.body
+            const {password, email} = req.body
             const user = new User(req.body)
 
+            //Prevenir duplicados
+
+            const userExists = await User.findOne({email})
+            if(userExists) {
+                const error = new Error('El usuario ya esta registrado en el sistema')
+                return res.status(409).json({error: error.message})
+            }
 
             // Hash password
             user.password = await hashPassword(password)
