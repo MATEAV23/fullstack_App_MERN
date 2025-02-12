66e51c3fefe40277b9f1388721632c69059d17dc
MATEAV23 <mateogalvarezvic23@gmail.com>
2025-02-11 23:46:40 -0300

    creando funcion para enviar mails

--- uptask_backend/src/controllers/AuthController.ts
+++ uptask_backend/src/controllers/AuthController.ts
@@ -3,6 +3,7 @@ import User from '../models/User'
 import { hashPassword } from '../utils/auth'
 import Token from '../models/Token'
 import { generateToken } from '../utils/token'
+import { transporter } from '../config/nodemailer'
 
 export class AuthController {
     
@@ -27,6 +28,18 @@ export class AuthController {
             const token = new Token()
             token.token = generateToken()
             token.user = user.id
+
+            // Enviar email
+
+            await transporter.sendMail({
+                from: 'UpTask <admin@uptask.com>',
+                to: user.email,
+                subject: 'UpTask - confirma tu cuenta',
+                text: 'Confirma tu cuenta en UpTask',
+                html: `
+                    <h1>Confirma tu cuenta en UpTask</h1>
+                `
+            })
 
             
             await Promise.allSettled([user.save(), token.save()])
