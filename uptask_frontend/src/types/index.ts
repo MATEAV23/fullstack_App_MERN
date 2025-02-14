4f6625642f91475c7576e2205a1641be04190d49
MATEAV23 <mateogalvarezvic23@gmail.com>
2025-02-14 18:28:23 -0300

    creando vista del formulario de login

--- uptask_frontend/src/types/index.ts
+++ uptask_frontend/src/types/index.ts
@@ -1,5 +1,17 @@
 import { z } from 'zod'
 
+/** Auth Users */
+
+const authSchema = z.object({
+    name: z.string(),
+    email: z.string(),
+    password: z.string(),
+    password_confirmation: z.string()
+})
+
+type Auth = z.infer<typeof authSchema>
+export type UserLoginForm = Pick<Auth, 'email' | 'password'>
+
 
 /** Tasks */ 
 
