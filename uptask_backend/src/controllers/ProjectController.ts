a145f8ae470d8f9669baad06e93a4d334fb7186c
MATEAV23 <mateogalvarezvic23@gmail.com>
2025-04-25 15:59:52 -0300

    Agregando identificador del usuario al request

--- uptask_backend/src/controllers/ProjectController.ts
+++ uptask_backend/src/controllers/ProjectController.ts
@@ -4,9 +4,11 @@ import Project from '../models/Project'
 export class ProjectController {
 
     static createProject = async (req: Request, res: Response) => {
+        const project = new Project(req.body)
 
+        console.log(req.user)
         try {
-            await Project.create(req.body)
+            await project.save()
             res.send('Proyecto creado correctamente')
         } catch (error) {
             console.log(error)
--- uptask_backend/src/middleware/auth.ts
+++ uptask_backend/src/middleware/auth.ts
@@ -1,6 +1,14 @@
 import jwt from "jsonwebtoken"
 import { Request, Response, NextFunction } from 'express'
-import User from "../models/User"
+import User, { IUser } from "../models/User"
+
+declare global {
+    namespace Express {
+        interface Request {
+            user?: IUser
+        }
+    }
+}
 
 export const authenticate = async (req: Request, res: Response, next: NextFunction) => {
     const bearer = req.headers.authorization
@@ -15,8 +23,13 @@ export const authenticate = async (req: Request, res: Response, next: NextFuncti
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
         if(typeof decoded === 'object' && decoded.id)
         {
-            const user = await User.findById(decoded.id)
-            console.log(user)
+            const user = await User.findById(decoded.id).select('_id name email')
+            if(user)
+            {
+                req.user = user
+            } else {
+                res.status(500).json({error: 'Usuario no encontrado'})
+            }
         }
         
     } catch (error) {
